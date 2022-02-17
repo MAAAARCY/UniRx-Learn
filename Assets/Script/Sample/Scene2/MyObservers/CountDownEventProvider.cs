ce5e3433b5f6dead34592a85ef3a58366b28a14c
MAAAAARCY <masatarou.1003@icloud.com>
2022-02-17 23:07:38 +0900

    learn beginning

---
+++ Assets/Script/Sample/Scene2/MyObservers/CountDownEventProvider.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+
+namespace Samples.Section2.MyObservers
+{
+    public class CountDownEventProvider : MonoBehaviour
+    {
+        [SerializeField]
+        private int _countSeconds = 10;
+
+        //Instance of Subject
+        private Subject<int> _subject;
+
+        //public interface of Subject
+        public IObservable<int> CountDownObservable => _subject;
+
+        private void Awake()
+        {
+            _subject = new Subject<int>();
+
+            StartCoroutine(CountCoroutine());
+        }
+
+        private IEnumerator CountCoroutine()
+        {
+            var current = _countSeconds;
+
+            while (current > 0)
+            {
+                _subject.OnNext(current);
+                current--;
+                yield return new WaitForSeconds(1.0f);
+            }
+
+            _subject.OnNext(0);
+            _subject.OnCompleted();
+        }
+
+        private void OnDestroy()
+        {
+            _subject.Dispose();
+        }
+    }
+}
---
+++ Assets/Script/Sample/Scene2/MyObservers/PrintLogObserver.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Samples.Section2.MyObservers
+{
+    public class PrintLogObserver<T> : IObserver<T>
+    {
+        public void OnCompleted()
+        {
+            UnityEngine.Debug.Log("OnCompleted!");
+        }
+
+        public void OnError(Exception error)
+        {
+            UnityEngine.Debug.LogError(error);
+        }
+
+        public void OnNext(T value)
+        {
+            UnityEngine.Debug.Log(value);
+        }
+    }
+}
